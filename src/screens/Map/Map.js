f167d9dcb7784c4f7368d6b31d00377b4a6ad9ac
namtx <dekapro9x@gmail.com>
2020-09-16 17:50:56 +0900

    Update Map IOS

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -1,12 +1,34 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {View, StyleSheet} from 'react-native';
+import {AppContainer} from '../../elements';
+import {COLOR} from '../../utils';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+});
 
-export default class Map extends Component {
-  render() {
-    return (
-      <View>
-        <Text> Map </Text>
-      </View>
-    );
-  }
-}
+export default () => (
+  <AppContainer
+    haveTitle
+    nameScreen={'Map'}
+    goBackScreen
+    style={{backgroundColor: COLOR.milk}}>
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}></MapView>
+  </AppContainer>
+);
