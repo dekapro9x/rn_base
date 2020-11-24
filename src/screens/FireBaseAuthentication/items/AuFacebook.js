6eabbef4a59e773f58408d1fa1e44ea8bbb287be
namtx <dekapro9x@gmail.com>
2020-11-24 21:38:34 +0900

    Config Environment  https://github.com/facebook/facebook-ios-sdk.

--- src/screens/FireBaseAuthentication/items/AuFacebook.js
+++ src/screens/FireBaseAuthentication/items/AuFacebook.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {View, TouchableOpacity} from 'react-native';
+import {COLOR, SIZE} from '../../../utils';
 
-export default function AuFacebook() {
+export default function AuFacebook(props) {
+  const {unShowModal} = props;
+
+  const pressActiveAccountDemo = () => {};
+  //Nút tắt modal và nút tự động get tài khoản:
+  const headerButtonModalAndGetAccount = () => {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          onPress={unShowModal}
+          style={{
+            height: SIZE.width(15),
+            width: SIZE.width(15),
+          }}>
+          <AntDesign
+            name={'closesquare'}
+            color={COLOR.grey_300}
+            size={45}></AntDesign>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={pressActiveAccountDemo}
+          style={{
+            height: SIZE.width(15),
+            width: SIZE.width(15),
+            marginLeft: SIZE.width(70),
+          }}>
+          <AntDesign
+            name={'login'}
+            color={COLOR.grey_300}
+            size={45}></AntDesign>
+        </TouchableOpacity>
+      </View>
+    );
+  };
   return (
     <View>
-      <Text> textInComponent </Text>
+      {headerButtonModalAndGetAccount()}
+      <View style={{flex: 1, backgroundColor: COLOR.white}}></View>
     </View>
   );
 }
---
+++ src/screens/FireBaseAuthentication/node/node/nodeLoginWithFacebook.md
@@ -0,0 +1,14 @@
+Login tài khoản fb với: react-native-fbsdk
+B1: Cài cắm thư viện fb sdk:
+
+# yarn add react-native-fbsdk
+
+# B1: Vào https://developers.facebook.com/
+
+Các bước đăng kí trong phần hình ảnh.
+Sau khi đăng kí thành công bắt đầu thiết lập môi trường phát triển.
+Làm theo các bước sau:
+https://developers.facebook.com/apps/921259178281044/fb-login/quickstart/
+Cài đặt Xcode:
+https://developers.facebook.com/docs/ios/getting-started/?sdk=
+Nhớ đoạn active link : https://github.com/facebook/facebook-ios-sdk chứ không phải https://github.com/facebook/facebook-ios-sdk.(thừa dấu . nên Xcode không active)
