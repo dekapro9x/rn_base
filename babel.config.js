4454b8fd3c731311c88795f7fcd89c640468ca2c
namtx <dekapro9x@gmail.com>
2021-01-20 13:05:06 +0900

    Config babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          '@components': './src/components',
+          '@fetchApi': './src/utils/modules/FetchApi',
+          '@resources': './src/utils/resources',
+          '@container': './src/components/Container',
+          '@screen': './src/screen/',
+          '@src': './src',
+        },
+      },
+    ],
+    'jest-hoist',
+  ],
 };
